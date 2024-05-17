d1317ba0b90d31056c0e98d202f1518114af79ac
ughru <ganhuiru@gmail.com>
2024-05-17 20:44:13 +0800

    record update
    different hook to play different record's audio -> may need to edit / find other solution for this

--- Assets/_Course Library/Scripts/SpinRecord.cs
+++ Assets/_Course Library/Scripts/SpinRecord.cs
@@ -2,49 +2,25 @@ using UnityEngine;
 
 public class SpinRecord : MonoBehaviour
 {
-    public float maxSpeed = 100f; // Maximum speed in degrees per second
-    public float acceleration = 30f; // How quickly the record reaches max speed
-    private float currentSpeed = 0f;
-    private bool isSpinning = false;
+    public float rotationSpeed = 10f;
 
-    // Update is called once per frame
-    void Update()
+    private bool isRotating = false;
+
+    public void StartRotation()
     {
-        // Check if the record should be spinning
-        if (isSpinning && currentSpeed < maxSpeed)
+        isRotating = true;
+    }
+
+    public void StopRotation()
+    {
+        isRotating = false;
+    }
+
+    private void Update()
+    {
+        if (isRotating)
         {
-            // Gradually increase the speed
-            currentSpeed += acceleration * Time.deltaTime;
-            if (currentSpeed > maxSpeed)
-            {
-                currentSpeed = maxSpeed;
-            }
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
         }
-        else if (!isSpinning && currentSpeed > 0)
-        {
-            // Gradually decrease the speed
-            currentSpeed -= acceleration * Time.deltaTime;
-            if (currentSpeed < 0)
-            {
-                currentSpeed = 0;
-            }
-        }
-
-        // Rotate the record around its Y-axis
-        transform.Rotate(Vector3.up, currentSpeed * Time.deltaTime);
     }
-
-    // Method to start spinning the record
-    public void StartSpinning()
-    {
-        isSpinning = true;
-        Debug.Log("Record spinning started.");
-    }
-
-    public void StopSpinning()
-    {
-        isSpinning = false;
-        Debug.Log("Record spinning stopped.");
-    }
-
 }
